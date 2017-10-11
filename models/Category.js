f1eb1296421a53f4dd22abb856aad6955dc5e3f4
sgdreamer7 <vns.scherbina@gmail.com>
2017-10-11 11:27:32 +0300

    Category.js and categories.js promisifying done

--- models/Category.js
+++ models/Category.js
@@ -13,27 +13,39 @@ var CategorySchema = new mongoose.Schema({
 CategorySchema.plugin(uniqueValidator, { message: 'is already taken' });
 
 CategorySchema.pre('validate', function (next) {
-  if (!this.slug) {
-    this.slugify();
-  }
-  if (!this.subcategories) {
-    this.subcategories = [];
-  }
+  if (!this.slug) { this.slugify(); }
+  if (!this.subcategories) { this.subcategories = []; }
   next();
 });
 
-CategorySchema.methods.slugify = function () {
-  this.slug = slug(this.title);
+CategorySchema.methods.slugify = function () { this.slug = slug(this.title); };
+
+CategorySchema.methods.populatePromised = function () {
+  return new Promise((resolve, reject) => {
+    this
+      .populate('subcategories')
+      .execPopulate()
+      .then(c => { resolve(c); })
+      .catch(err => reject(err));
+  });
 };
 
-CategorySchema.methods.toJSON = function () {
-  return {
-    slug: this.slug,
-    title: this.title,
-    subcategories: this.subcategories,
-    createdAt: this.createdAt,
-    updatedAt: this.updatedAt
-  }
+CategorySchema.methods.toJSONPopulatedPromised = function () {
+  return new Promise((resolve, reject) => {
+    this
+      .populate('subcategories')
+      .execPopulate()
+      .then(category => {
+        resolve({
+          slug: category.slug,
+          title: category.title,
+          subcategories: category.subcategories,
+          createdAt: category.createdAt,
+          updatedAt: category.updatedAt
+        });
+      })
+      .catch(err => reject(err));
+  })
 };
 
 mongoose.model('Category', CategorySchema);
--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,135 +1,126 @@
-var router = require('express').Router();
-var mongoose = require('mongoose');
-var Category = mongoose.model('Category');
-var User = mongoose.model('User');
-var auth = require('../auth');
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const Category = mongoose.model('Category');
+const User = mongoose.model('User');
+const auth = require('../auth');
 
-router.param('category', function (req, res, next, slug) {
-  Category.findOne({ slug: slug })
-    .then(function (category) {
-      if (!category) { return res.sendStatus(404); }
-
+// Prepare :category parameter
+router.param('category', (req, res, next, slug) => {
+  Category
+    .findOne({ slug: slug })
+    .then(category => {
+      if (!category) return res.sendStatus(404);
       req.category = category;
-
       return next();
-    }).catch(next);
-});
-
-router.param('subcategory', function (req, res, next, slug) {
-  Category.findOne({ slug: slug })
-    .then(function (category) {
-      if (!category) { return res.sendStatus(404); }
-
-      req.subcategory = category;
-
-      return next();
-    }).catch(next);
-});
-
-router.get('/', auth.optional, function (req, res, next) {
-  var query = {};
-  return Promise.all([
-    Category.find(query)
-      .sort({ title: 'asc' })
-      .populate('subcategories')
-      .exec(),
-    Category.count(query).exec()
-  ]).then(function (results) {
-    var categories = results[0];
-    var categoriesCount = results[1];
-    return res.json({
-      categories: categories,
-      categoriesCount: categoriesCount
-    });
-  }).catch(next);
-});
-
-router.post('/', auth.required, function (req, res, next) {
-  var category = new Category(req.body.category);
-  return category.save().then(function () {
-    return category.populate('subcategories').execPopulate()
-      .then(function (c) {
-        return res.json({ category: c.toJSON() });
-      }).catch(next);
-  }).catch(next);
-});
-
-router.get('/:category', auth.optional, function (req, res, next) {
-  return req.category.populate('subcategories').execPopulate()
-    .then(function (c) {
-      return res.json({ category: c.toJSON() });
-    }).catch(next);
-});
-
-router.put('/:category', auth.required, function (req, res, next) {
-  if (typeof req.body.category.title !== 'undefined') {
-    req.category.title = req.body.category.title;
-  }
-
-  req.category.save().then(function (category) {
-    return req.category.populate('subcategories').execPopulate()
-      .then(function (c) {
-        return res.json({ category: c.toJSON() });
-      }).catch(next);
-  }).catch(next);
-});
-
-router.delete('/:category', auth.required, function (req, res, next) {
-  removeFromAllSubcategory()
-  Category.find({subcategories: {$all:[req.category._id]}})
-  .exec()
-  .then(function(results) {
-    Promise.all(results.map(function(c) {
-      c.subcategories = c.subcategories.filter(function (s) {
-        return !(s.toHexString() === req.category._id.toHexString())
-      });
-      return c.save();
-    }))
-    .then(function() {
-      req.category.remove()
     })
-    .then(function() {
-      return res.sendStatus(204);
-    });
-  })
-  .catch(next);
+    .catch(next);
 });
 
-router.post('/:category/subcategories/:subcategory', auth.required, function (req, res, next) {
-  return req.category.populate('subcategories').execPopulate()
-    .then(function (c) {
-      var indx = c.subcategories.findIndex(function (element, index, array) {
-        return element.slug === req.subcategory.slug
-      });
+// Prepare :subcategory parameter
+router.param('subcategory', (req, res, next, slug) => {
+  Category
+    .findOne({ slug: slug })
+    .then(category => {
+      if (!category) return res.sendStatus(404);
+      req.subcategory = category;
+      return next();
+    })
+    .catch(next);
+});
+
+// Get all categories
+router.get('/', auth.optional, (req, res, next) => {
+  const query = {};
+  Category
+    .find(query)
+    .sort({ title: 'asc' })
+    .exec()
+    .then(categories => {
+      return Promise.all(categories.map(category => {
+        return category.toJSONPopulatedPromised()
+      }));
+    })
+    .then(categories => { return res.json({ categories: categories }); })
+    .catch(next);
+});
+
+// Add new category
+router.post('/', auth.required, (req, res, next) => {
+  new Category(req.body.category)
+    .save()
+    .then(category => { return category.toJSONPopulatedPromised(); })
+    .then(category => { return res.json({ category: category }) })
+    .catch(next);
+});
+
+// Get selected category
+router.get('/:category', auth.optional, (req, res, next) => {
+  req.category
+    .toJSONPopulatedPromised()
+    .then(category => { return res.json({ category: category }); })
+    .catch(next);
+});
+
+// Update category title
+router.put('/:category', auth.required, (req, res, next) => {
+  if (typeof req.body.category.title !== 'undefined') req.category.title = req.body.category.title;
+  req.category
+    .save()
+    .then(category => { return category.toJSONPopulatedPromised(); })
+    .then(category => { return res.json({ category: category }); })
+    .catch(next);
+});
+
+// Delete selected category
+router.delete('/:category', auth.required, (req, res, next) => {
+  Category
+    .find({ subcategories: { $all: [req.category._id] } })
+    .exec()
+    .then(results => {
+      return Promise.all(results.map(category => {
+        category.subcategories = category.subcategories.filter(subcategories => {
+          return !(subcategories.toHexString() === req.category._id.toHexString())
+        });
+        return category.save();
+      }));
+    })
+    .then(() => { req.category.remove() })
+    .then(() => { return res.sendStatus(204); })
+    .catch(next);
+});
+
+
+// Add subcategory to selected category
+router.post('/:category/subcategories/:subcategory', auth.required, (req, res, next) => {
+  return req.category.populatePromised()
+    .then(c => {
+      var indx = c.subcategories.findIndex((element, index, array) => { return element.slug === req.subcategory.slug });
       if (indx === -1) {
         req.category.subcategories.push(req.subcategory);
-        return req.category.save().then(function (c2) {
-          return res.json({ category: c2.toJSON() });
-        });
+        return req.category
+          .save()
+          .then(category => { return category.toJSONPopulatedPromised(); })
+          .then(category => { return res.json({ category: category }); })
+          .catch(next)
       }
-      return res.json({ category: c.toJSON() });
-    }).catch(next);
-
-  req.category.populate('subcategories').execPopulate().then(function (category) {
-
-  }).catch(next);
+      return category
+        .toJSONPopulatedPromised()
+        .then(category => { return res.json({ category: category }); })
+        .catch(next);
+    })
+    .catch(next);
 });
 
-router.delete('/:category/subcategories/:subcategory', auth.required, function (req, res, next) {
-  req.category.subcategories = req.category.subcategories.filter(function (element) {
+// Remove subcategory from selected category
+router.delete('/:category/subcategories/:subcategory', auth.required, (req, res, next) => {
+  req.category.subcategories = req.category.subcategories.filter(element => {
     return !(element.toHexString() === req.subcategory._id.toHexString())
   });
-  return req.category.save().then(function (c) {
-    return c.populate('subcategories').execPopulate()
-      .then(function (c2) {
-        return res.json({ category: c2.toJSON() });
-      }).catch(next);
-  }).catch(next);
+  return req.category
+    .save()
+    .then(category => { return category.toJSONPopulatedPromised(); })
+    .then(category => { return res.json({ category: category }); })
+    .catch(next);
 });
-
-function removeFromAllSubcategory (subcategory) {
-
-};
-
 
 module.exports = router;
